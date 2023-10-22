13e6a550ec3cdcb33f4c9cea1e680da4da294a67
Klaus Reuter <klaus.reuter@mpcdf.mpg.de>
2023-10-22 20:48:07 +0200

    version bump to 0.1.3, adding activate scripts

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (base_dir / "README.md").read_text()
 
 setup(
     name='condainer',
-    version='0.1.2',
+    version='0.1.3',
     description='Build, manage, and run compressed squashfs images of Conda environments transparently on HPC or elsewhere.',
     long_description = long_description,
     author='Klaus Reuter',
